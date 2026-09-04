e3e5f5708f93c3d955699d497fe270bf0c2327ce
klkmoraa <crisdlm302@gmail.com>
2026-09-04 11:01:08 -0600

    feat: link Solver 2D to its public deployment

---
+++ src/productLinks.ts
@@ -0,0 +1,5 @@
+/** Public entry points for independently deployed FusionStructure products. */
+export const PRODUCT_LINKS = {
+  fstructure: 'https://klkmoraa.github.io/fstructure/',
+  space3d: 'https://github.com/klkmoraa/fusionstructure-space3d',
+} as const;
